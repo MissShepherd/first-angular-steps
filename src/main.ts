36e1a86a36ac755bbfb7fb97628d9acb23081684
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-10-27 09:06:27 +0100

    refactor: simplify AppComponent by removing unused lifecycle hooks; update gif display logic and styling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,42 +9,23 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
       <div class="card">
         <h2>Example Card</h2>
-        <p>This is a simple card with a bit of CSS styling.</p>
+        <p>Watch Gudetama move around!</p>
 
-        <img
-          class="gif"
-          [src]="gifUrl + cacheBuster"
-          alt="looped gif"
-        />
+        <div class="gif-container">
+          <img
+            class="gudetama"
+            src="https://i.pinimg.com/originals/d7/a0/9f/d7a09ffd64f946e0087ed82b8c7a4665.gif"
+            alt="Gudetama"
+          />
+        </div>
 
-        <br />
         <input #box type="text" placeholder="Type something..." />
         <button class="btn" (click)="onClick()">Click Me</button>
       </div>
     </div>
   `
 })
-export class AppComponent implements OnInit, OnDestroy {
-  gifUrl =
-    'https://i.pinimg.com/originals/d7/a0/9f/d7a09ffd64f946e0087ed82b8c7a4665.gif';
-  cacheBuster = '';
-  loopTime = 3000; // milliseconds (adjust to match gif length)
-  private timer?: number;
-
-  ngOnInit() {
-    this.startLoop();
-  }
-
-  ngOnDestroy() {
-    if (this.timer) clearInterval(this.timer);
-  }
-
-  startLoop() {
-    this.timer = window.setInterval(() => {
-      this.cacheBuster = `?t=${Date.now()}`; // reloads the gif
-    }, this.loopTime);
-  }
-
+export class AppComponent {
   onClick() {
     console.log('Button clicked');
   }
